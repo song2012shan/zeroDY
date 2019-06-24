c00ae2a4f45a3860db60c70b3628008b3486faa5
song2012shan <song2012shan@yahoo.co.jp>
2019-06-24 15:26:06 +0900

    初始化程序

---
+++ dy1/dy1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dy1
+//
+//  Created by 孙红艳 on 2019/06/24.
+//  Copyright © 2019 孙红艳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
